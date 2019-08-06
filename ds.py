cf10366b6e0eaa328a0720a2be7e8b2dfec5168e
Bairn <624544041@qq.com>
2019-08-07 00:07:58 +0800

    Enter

--- ds.py
+++ ds.py
@@ -1,13 +1,19 @@
-
-
-
-
 class txn:
     def __init__(self):
         self.proof = []
         self.count = 0
         self.becounted = 0
 
+
 class value:
-    def __init__(self):
-        self.
+
+    def __init__(self, ori_node):
+        self.owner = ori_node
+        self.proof = []
+        self.ownproof = []
+
+    def change_own(self, new_node):
+        self.owner = new_node
+
+    def add_proof(self, newproof):
+        self.proof.append(newproof)
