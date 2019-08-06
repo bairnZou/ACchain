ceccd1081465c705ef09c626ac33c72f3bdb8313
Bairn <624544041@qq.com>
2019-08-07 00:27:17 +0800

    Enter

--- ds.py
+++ ds.py
@@ -1,28 +1,32 @@
 class txn:
-    def __init__(self, index):
-        self.proof_value = index
-        self.count = 0
-        self.becounted = 0
+        proof_value = 0
+        count = 0
+        becounted = False
+        def __init__(self, index):
+            self.proof_value = index
 
 
 class TXN:
+
     def __init__(slef, N):
         slef.txn = 
 
 
-
 class value:
+        owner = 0
+        proof = []
+        ownproof = []
+        def __init__(self, ori_node):
+            self.owner = ori_node
 
-    def __init__(self, ori_node):
-        self.owner = ori_node
-        self.proof = []
-        self.ownproof = []
+        def change_own(self, new_node):
+            self.owner = new_node
 
-    def change_own(self, new_node):
-        self.owner = new_node
+        def add_proof(self, newproof):
+            self.proof.append(newproof)
+    
+        def change_ownproof(self,):
 
-    def add_proof(self, newproof):
-        self.proof.append(newproof)
 
 class Node:
 
