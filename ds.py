8ae0314b56c92a913135d8eba6d73bfeca7d7a63
Bairn <624544041@qq.com>
2019-08-06 23:56:07 +0800

    Enter

---
+++ ds.py
@@ -0,0 +1,13 @@
+
+
+
+
+class txn():
+    def __init__(self):
+        self.proof = []
+        self.count =  0
+        self.becounted = 0
+
+class value():
+    def __init__(self):
+        self.
